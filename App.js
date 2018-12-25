3d9ac000a4163fbb4709cc0ded88e8b03ae34178
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-12-24 23:45:38 -0800

    Added button example from Expo docs

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/buttons'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>OMG A MOBILE APP</Text>
+        <Button/>
       </View>
     );
   }
